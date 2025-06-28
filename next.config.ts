982bf8f068cdc492d476a63121c9b286e77a3e79
zenagui bilal <zenaguibilal2@gmail.com>
2025-06-28 16:16:51 +0000

    Error: Invalid src prop (https://placehold.co/400x400.png) on `next/imag

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
