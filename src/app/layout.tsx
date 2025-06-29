be496c8778b9aaae6be2e4be40c2e4cfed5db929
zenagui bilal <zenaguibilal2@gmail.com>
2025-06-29 00:39:04 +0000

    التنقل بين الصفحات بطيء

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { AppLayout } from '@/components/app-layout';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { GeistSans } from 'geist/font/sans';
 import { cn } from '@/lib/utils';
+import NextTopLoader from 'nextjs-toploader';
 
 export const metadata: Metadata = {
   title: 'Frucio',
@@ -21,6 +22,17 @@ export default function RootLayout({
     <html lang="en" className={GeistSans.variable} suppressHydrationWarning>
       <head />
       <body className="font-sans antialiased">
+        <NextTopLoader
+          color="#008080"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #008080,0 0 5px #008080"
+        />
         <AppProviders>
           <SidebarProvider>
             <AppLayout>{children}</AppLayout>
