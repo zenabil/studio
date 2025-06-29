ee8500606d1742036a726f4a3dcce2db31d4db2f
zenagui bilal <zenaguibilal2@gmail.com>
2025-06-29 00:55:19 +0000

    TypeError: plugin is not a function     at <unknown> (file:///home/user/

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,11 +3,11 @@ import {genkit} from 'genkit';
 
 // Using require for better CJS/ESM interop in Next.js server environment.
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { ollama } = require('genkitx-ollama');
+const ollamaPlugin = require('genkitx-ollama');
 
 export const ai = genkit({
   plugins: [
-    ollama({
+    ollamaPlugin.ollama({
       models: [{name: 'gemma:2b'}],
       serverAddress: 'http://127.0.0.1:11434',
     }),
