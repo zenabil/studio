5b089a8825cc8ab0a72497961f5192d1c1910178
zenagui bilal <zenaguibilal2@gmail.com>
2025-11-10 19:23:27 +0000

    اكتب جميع API KEY في ملف ENV

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "mercurio-pos",
   "appId": "1:504678797370:web:c4172618c0bc5dc73aa503",
-  "apiKey": "AIzaSyApCgPFrniIQuaWRHUeXa5xptB2Y_xHpQE",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "mercurio-pos.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "504678797370"
