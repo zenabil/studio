eb6ef1da1e74f85ab9824999df989cf70ed03868
zenagui bilal <zenaguibilal2@gmail.com>
2025-11-13 09:43:03 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { onCall, HttpsError, CallableRequest } from "firebase-functions/v2/https";
+import { onCall, HttpsError, type CallableRequest } from "firebase-functions/v2/https";
 import { getAuth } from "firebase-admin/auth";
 
 initializeApp();
